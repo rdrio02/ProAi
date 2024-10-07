1babdb990fcdae2ab72f2549352cb4087bbe6c84
rdrio02 <rodrigo.laureano090@gmail.com>
2024-10-07 14:01:17 +0200

    Submit Earthquake files

--- dataProvider.py
+++ dataProvider.py
@@ -3,8 +3,9 @@ import streamlit as st
 
 data = pd.read_csv("earthquakes.csv")
 
-st.title("Global Earthquake Data")
+st.header("Global Earthquake Data")
 st.write("Comprehensive datasets of global earthquake with key attributes for analysis:")
 #I have still to write the link to the keggle website
-
-st.write(data)
+st.write(data.head(5))
+st.subheader("Magnitude")
+st.write("Min: ", data.iloc[2].min())
