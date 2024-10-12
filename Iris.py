602dbaf9164c9186a63ba3c1432946b9c2b13530
Rdrio <rodrigo.laureano090@gmail.com>
2024-10-12 13:17:13 +0200

    update

--- Iris.py
+++ Iris.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 dataURL = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 columnNames = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-df = pd.read_csv(data_url, header=None, names=column_names)
+df = pd.read_csv(dataURL, header=None, names=columnNames)
 
 st.title("Iris Data")
 st.write("Here are the first 5 data rows")
