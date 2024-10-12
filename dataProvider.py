0e46e7c0b778d640dd07660092b199b3cd1e65f1
Rdrio <rodrigo.laureano090@gmail.com>
2024-10-12 13:04:50 +0200

    update

--- dataProvider.py
+++ dataProvider.py
@@ -6,7 +6,7 @@ data = pd.read_csv("earthquakes.csv")
 st.title("Global Earthquake Data")
 st.write("Comprehensive datasets of global earthquake with key attributes for analysis:")
 #I have still to write the link to the keggle website
-st.write(data.head(5))
+st.write(data)
 
 st.header("Magnitude")
 st.write("Min: ", data["magnitude"].min())
