2a7cae6e29f3dd8c8df564559721b62fe54e034a
rdrio02 <rodrigo.laureano090@gmail.com>
2024-10-14 14:07:37 +0200

    Submit Earthquake files

--- data_generation.py
+++ data_generation.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib as plt
-import seaborn as sb
+#import numpy as np
+#import matplotlib as plt
+#import seaborn as sb
 
 firstNames = pd.read_csv("first_names.csv")
 lastNames = pd.read_csv("last_names.csv")
