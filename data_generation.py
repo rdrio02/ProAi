0bc56044b1c6bef69c171d9b0773d973283f58ee
rdrio02 <rodrigo.laureano090@gmail.com>
2024-10-14 14:06:04 +0200

    Submit Earthquake files

---
+++ data_generation.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sb
+
+firstNames = pd.read_csv("first_names.csv")
+lastNames = pd.read_csv("last_names.csv")
+subjects = pd.read_csv("subjects.csv")
+
+@st.fragment
+def fragment_function():
+    st.write("Hello")
