975a1c1613b9306f8ef5af4a6a5f90f7a063c213
rdrio02 <rodrigo.laureano090@gmail.com>
2024-10-14 14:32:01 +0200

    Submit Earthquake files

--- data_generation.py
+++ data_generation.py
@@ -8,11 +8,20 @@ firstNames = pd.read_csv("first_names.csv")
 lastNames = pd.read_csv("last_names.csv")
 subjects = pd.read_csv("subjects.csv")
 
-col1, col2 = st.columns(2)
 
+df = []
+
+col1, col2 = st.columns(2)
 with st.sidebar:
-    number = st.number_input("Insert a number")
-    st.write(number)
+    numberStudents = st.number_input("Number of students")
+    numberSubjects = st.number_input("Number of subjects")
+    gradesPerSubject = st.number_input("Grades per subject")
+    minimumGrade = st.number_input("Minimum grade")
+    maximumGrade = st.number_input("Maximum grade")
+    selectedStudent = st.selectbox("Select student", df['Student'].unique())
+
+
+
 
 with col1:
     st.write("s")
