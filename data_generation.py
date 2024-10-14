851416c77b842265e835c9abbc0dc5d0845cf077
Rdrio <rodrigo.laureano090@gmail.com>
2024-10-14 20:05:30 +0200

    updating

--- data_generation.py
+++ data_generation.py
@@ -1,18 +1,10 @@
-from calendar import month_name
-
 import streamlit as st
 import pandas as pd
 #import numpy as np
 #import matplotlib as plt
 #import seaborn as sb
 
-firstNames = pd.read_csv("first_names.csv")
-lastNames = pd.read_csv("last_names.csv")
-subjects = pd.read_csv("subjects.csv")
 
-
-df = pd.concat([firstNames, lastNames, subjects], axis=0, join='outer')
-st.write(df)
 
 col1, col2 = st.columns(2)
 with st.sidebar:
@@ -25,7 +17,15 @@ with st.sidebar:
 
 
 
+firstNames = pd.read_csv("first_names.csv")
+lastNames = pd.read_csv("last_names.csv")
+subjects = pd.read_csv("subjects.csv")
 
+
+
+
+df = pd.concat([firstNames, lastNames, subjects], axis=0, join='outer')
+st.write(df)
 
 
 
