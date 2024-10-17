cfad4fc5b524e665f1ef6f4733902f11eb8148b6
Rdrio <rodrigo.laureano090@gmail.com>
2024-10-17 17:25:24 +0200

    updating

--- data_generation.py
+++ data_generation.py
@@ -27,7 +27,7 @@ selectedFirstNames = np.random.choice(firstNames.squeeze(), numberStudents, repl
 selectedLastNames = np.random.choice(lastNames.squeeze(), numberStudents, replace=False)
 sortedLastNames = pd.Series(selectedLastNames).sort_values()
 
-nameList = selectedFirstNames + " " + selectedLastNames
+nameList = selectedFirstNames + " " + sortedLastNames
 
 st.write(nameList)
 
