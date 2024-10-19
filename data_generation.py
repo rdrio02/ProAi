50a6f06d92d69c71b2e254e294c86e12d17d2881
Rdrio <rodrigo.laureano090@gmail.com>
2024-10-19 14:25:11 +0200

    updating

--- data_generation.py
+++ data_generation.py
@@ -59,9 +59,7 @@ with st.sidebar:
 
 
 
-# Create histograms using Plotly
-filtered_data = data[data['Name'] == selectedStudent]
-fig_plotly_histogram = px.histogram(filtered_data,
+fig_plotly_histogram = px.histogram(data,
                                     x='Grade',
                                     color='Subject',  # Color by subject
                                     nbins=10,
