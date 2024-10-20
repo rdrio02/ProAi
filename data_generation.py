3e481eadcb9a5e37f3852b90222a7f1fee7ab528
Rdrio <rodrigo.laureano090@gmail.com>
2024-10-20 08:57:00 +0200

    updating

--- data_generation.py
+++ data_generation.py
@@ -72,7 +72,7 @@ fig_plotly_histogram = px.histogram(data,
 fig_seaborn, ax = plt.subplots(figsize=(8, 5))
 
 # Loop through each subject and plot a separate KDE with a different color
-for subject in filtered_data['Subject'].unique():
+for subject in data['Subject'].unique():
     subject_data = filtered_data[filtered_data['Subject'] == subject]
     sns.kdeplot(subject_data['Grade'], fill=True, ax=ax, label=subject, clip=(minimumGrade, maximumGrade))
 
