8821d9d8ff1771287bb867a5038973a2d561197a
rdrio02 <rodrigo.laureano090@gmail.com>
2024-10-21 12:59:36 +0200

    update

--- dataGeneration.py
+++ dataGeneration.py
@@ -53,16 +53,10 @@ with st.sidebar:
                                                        value=st.session_state['maximumGrade'],
                                                        step=1)
 
-# Ensure the number of subjects and names do not exceed the available list sizes
-number_students = min(st.session_state['numberStudents'], len(firstNames), len(lastNames))
-number_subjects = min(st.session_state['numberSubjects'], len(subjects))
-
 # Select random subjects and names
-selected_subjects = random.sample(subjects, number_subjects)
-selected_firstNames = random.sample(firstNames, number_students)
-selected_lastNames = random.sample(lastNames, number_students)
-
-# Create the full names
+selected_subjects = np.random.choice(subjects, st.session_state['numberSubjects'], replace=False)
+selected_firstNames = np.random.choice(firstNames, st.session_state['numberStudents'], replace=False)
+selected_lastNames = np.random.choice(lastNames, st.session_state['numberStudents'], replace=False)
 names = [f"{fn} {ln}" for fn, ln in zip(selected_firstNames, selected_lastNames)]
 
 # Generate student data
