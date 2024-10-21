f3c44a5ea842c0db165c99b77b5cf0de3db1a5fd
rdrio02 <rodrigo.laureano090@gmail.com>
2024-10-21 12:47:26 +0200

    update

--- dataGeneration.py
+++ dataGeneration.py
@@ -57,8 +57,10 @@ with st.sidebar:
                                                        value=st.session_state['maximumGrade'],
                                                        step=1)
 
+st.write(st.session_state['numberSubjects'])
+
 # Select random subjects and names
-selected_subjects = np.random.choice(subjects, int(st.session_state['numberSubjects']), replace=False)
+selected_subjects = np.random.choice(subjects, st.session_state['numberSubjects'], replace=False)
 selected_firstNames = np.random.choice(firstNames, st.session_state['numberStudents'], replace=False)
 selected_lastNames = np.random.choice(lastNames, st.session_state['numberStudents'], replace=False)
 names = [f"{fn} {ln}" for fn, ln in zip(selected_firstNames, selected_lastNames)]
